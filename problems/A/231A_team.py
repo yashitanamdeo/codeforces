0e743ac322e89fa8cda00745c2c7490f5e1c7ca6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-06 23:14:37 +0530

    add A. Team

--- problems/A/231A_team.py
+++ problems/A/231A_team.py
@@ -1,1 +1,8 @@
 # Problem Statement: https://codeforces.com/problemset/problem/231/A
+
+solve_count = 0
+for _ in range(int(input())):
+    petya,vasya,tonya = map(int,input().split())
+    if petya+vasya+tonya >=2:
+        solve_count += 1
+print(solve_count)
