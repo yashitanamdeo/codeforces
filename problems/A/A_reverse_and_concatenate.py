ce79a4f8734c7089865f8dec5cad22b279ef3982
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-06 23:18:14 +0530

    add A. Reverse and Concatenate

--- problems/A/A_reverse_and_concatenate.py
+++ problems/A/A_reverse_and_concatenate.py
@@ -1,1 +1,25 @@
 # Problem Statement: https://codeforces.com/contest/1634/problem/A
+
+def isPalindrome(s):
+    if s == s[::-1]:
+        return True
+    else:
+        return False
+    
+t = int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    s = str(input())
+    
+    if k == 0:
+        print(1)
+    if k == 1:
+        if isPalindrome(s):
+            print(1)
+        else:
+            print(2)
+    if k > 1:
+        if isPalindrome(s):
+            print(1)
+        else:
+            print(2)
